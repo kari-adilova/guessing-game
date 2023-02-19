feb17adcda2c3cbcbbca554f80a54fc501e97cbc
Kari Adilova <kariadilovaadilova7@gmail.com>
2023-02-19 22:48:31 +0500

    feat: implement Guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,29 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        let array = [];
+        for (let i = min; i <= max - min; i++) {
+            array.push(i);
+        }
+        this.array = array;
+        this.min = 0;
+        this.max = array.length - 1;
     }
+    
 
     guess() {
+        let middle = Math.round((this.max + this.min) / 2);
+        this.middle = middle;
+        return this.array[middle];
 
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
